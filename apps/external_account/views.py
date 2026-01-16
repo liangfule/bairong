e9ee0e7c5326fe9207a6642181cebcad18534020
unknown <fule.liang@bairong.ad.com>
2026-01-16 21:55:43 +0800

    测试1

--- apps/external_account/views.py
+++ apps/external_account/views.py
@@ -10,6 +10,11 @@ from django.views.decorators.http import require_GET, require_POST
 
 from apps.external_account.models import WecomInfo
 
+#插件
+def plugin_call(request):
+    print(1)
+    print(2)
+    print(3)
 
 # 查询渠道账号是否绑定
 @require_GET
