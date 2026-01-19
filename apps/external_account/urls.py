49b5777d087869fcb428fc9e2b20ba9f6aa6f616
unknown <fule.liang@bairong.ad.com>
2026-01-19 17:54:00 +0800

    测试插件接口

--- apps/external_account/urls.py
+++ apps/external_account/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('query_wecom_account/', views.query_wecom_account),
-    path('del_wecom_account/', views.del_wecom_account)
+    path('del_wecom_account/', views.del_wecom_account),
+    path('test_function/', views.test_function)
 ]
